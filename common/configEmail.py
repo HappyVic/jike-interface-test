881adf69f6d27c3a1e7be515eb8d991ff1a3b911
Vic <8463299@qq.com>
2019-04-03 19:09:51 +0800

    no message

--- common/configEmail.py
+++ common/configEmail.py
@@ -1,5 +1,4 @@
 # coding:utf-8
-
 import os
 import smtplib
 from email.mime.text import MIMEText
--- common/url.py
+++ common/url.py
@@ -9,3 +9,15 @@ users_profile = "/1.0/users/profile"#个人主页
 users_register= "/1.0/users/register"#注册匿名帐号
 
 
+#主理人申请接口
+topics_roles_admin_checkQualification = "/1.0/topics/roles/admin/checkApplicationQualification?topicId=<topicId>" #检查用户的申请资格
+topics_roles_admin_submit = "/1.0/topics/roles/admin/submitApplication" #提交用户的主理人申请
+
+#客户端主理人相关接口
+topics_getDetail = "/1.0/topics/getDetail"#主题详情页
+topics_listInvolvedUsers
+
+
+#客户端动态详情页接口
+originalPosts_get = "/1.0/originalPosts/get?refTopicId=<topicId>" #主题详情页进入动态详情页
+comments_listPrimary = "/1.0/comments/listPrimary" #评论列表
